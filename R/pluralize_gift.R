baa1c39ab41ef3da0bffabd0ab0c30d94de37831
Elaine Pranadjaya <elainesabine@gmail.com>
2021-05-16 14:17:34 -0700

    Update pluralize_gift.R

--- R/pluralize_gift.R
+++ R/pluralize_gift.R
@@ -14,7 +14,7 @@
 
 pluralize_gift <- function(gift){
   gift <- case_when(
-    str_detect(gift, "s$") ~ str_replace(gift, "y$", "ies"),
+    str_detect(gift, "y$") ~ str_replace(gift, "y$", "ies"),
     str_detect(gift, "oo") ~ str_replace(gift, "oo", "ee"),
     TRUE ~ str_c(gift, "s"))
   return(gift)
